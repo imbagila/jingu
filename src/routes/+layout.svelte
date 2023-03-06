669e39e4852766980157e4c364750e21a371ca79
imbagila <roid.rabbani@gmail.com>
2023-03-06 21:55:11 +0700

    refactor: remove pre-defined css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import "../app.css";
-	import Header from './Header.svelte';
+	import Nav from './Nav.svelte';
+	import Sidebar from './Sidebar.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
+	<Nav />
+	<Sidebar />
 
 	<main>
 		<slot />
@@ -16,39 +18,5 @@
 	</footer>
 </div>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+<style lang="postcss">
 </style>
